07ae0db69c6295f7ee8416e95730e2014c5a4e11
Karl Wallenius <kgw-dev@pm.me>
2026-02-14 21:03:16 +0100

    update tests

--- templates/http-service/internal/server/server_test.go
+++ templates/http-service/internal/server/server_test.go
@@ -12,99 +12,100 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-// func TestNew(t *testing.T) {
-// 	tests := []struct {
-// 		name  string
-// 		input []Option
-// 		want  *server
-// 	}{
-// 		{
-// 			name:  "default",
-// 			input: []Option{},
-// 			want: &server{
-// 				httpServer: &http.Server{
-// 					Addr:         defaultHost + ":" + defaultPort,
-// 					Handler:      &router{ServeMux: http.NewServeMux()},
-// 					ReadTimeout:  defaultReadTimeout,
-// 					WriteTimeout: defaultWriteTimeout,
-// 					IdleTimeout:  defaultIdleTimeout,
-// 				},
-// 				router:          &router{ServeMux: http.NewServeMux()},
-// 				log:             defaultLogger(),
-// 				shutdownTimeout: defaultShutdownTimeout,
-// 				shutdownHook:    defaultShutdownHook,
-// 			},
-// 		},
-// 		{
-// 			name: "with options",
-// 			input: []Option{
-// 				WithOptions(Options{
-// 					Router:       NewRouter(),
-// 					Logger:       defaultLogger(),
-// 					Host:         "localhost",
-// 					Port:         8081,
-// 					ReadTimeout:  10 * time.Second,
-// 					WriteTimeout: 10 * time.Second,
-// 					IdleTimeout:  15 * time.Second,
-// 				}),
-// 			},
-// 			want: &server{
-// 				httpServer: &http.Server{
-// 					Addr:         "localhost:8081",
-// 					Handler:      &router{ServeMux: http.NewServeMux()},
-// 					ReadTimeout:  10 * time.Second,
-// 					WriteTimeout: 10 * time.Second,
-// 					IdleTimeout:  15 * time.Second,
-// 				},
-// 				router:          &router{ServeMux: http.NewServeMux()},
-// 				log:             defaultLogger(),
-// 				shutdownTimeout: defaultShutdownTimeout,
-// 				shutdownHook:    defaultShutdownHook,
-// 			},
-// 		},
-// 	}
-//
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			got := New(test.input...)
-// 			if got == nil {
-// 				t.Errorf("New(%v) = nil; want %v", test.input, test.want)
-// 			}
-//
-// 			if diff := cmp.Diff(
-// 				test.want,
-// 				got,
-// 				cmp.AllowUnexported(server{}),
-// 				cmpopts.IgnoreUnexported(http.Server{}, http.ServeMux{}, slog.Logger{}),
-// 				cmpopts.IgnoreFields(server{}, "shutdownHook"),
-// 			); diff != "" {
-// 				t.Errorf("New(%v) = unexpected result (-want +got):\n%s\n", test.input, diff)
-// 			}
-// 		})
-// 	}
-// }
-//
-// func TestServer_Start(t *testing.T) {
-// 	t.Run("start server", func(t *testing.T) {
-// 		shutdownCh := make(chan os.Signal)
-// 		go func() {
-// 			time.Sleep(time.Millisecond * 200)
-// 			shutdownCh <- syscall.SIGINT
-// 		}()
-//
-// 		var buf bytes.Buffer
-// 		srv := New(WithLogger(slog.New(slog.NewJSONHandler(&buf, nil))))
-// 		srv.shutdownHook = func() os.Signal {
-// 			return <-shutdownCh
-// 		}
-//
-// 		if gotErr := srv.Start(t.Context()); gotErr != nil {
-// 			t.Errorf("Start() = unexpected result, got error: %v\n", gotErr)
-// 		}
-// 	})
-// }
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []Option
+		want  *server
+	}{
+		{
+			name:  "default",
+			input: []Option{},
+			want: &server{
+				httpServer: &http.Server{
+					Addr:         defaultHost + ":" + defaultPort,
+					Handler:      &router{ServeMux: http.NewServeMux()},
+					ReadTimeout:  defaultReadTimeout,
+					WriteTimeout: defaultWriteTimeout,
+					IdleTimeout:  defaultIdleTimeout,
+				},
+				router:          &router{ServeMux: http.NewServeMux()},
+				log:             defaultLogger(),
+				shutdownTimeout: defaultShutdownTimeout,
+				shutdownHook:    defaultShutdownHook,
+			},
+		},
+		{
+			name: "with options",
+			input: []Option{
+				WithOptions(Options{
+					Router:       NewRouter(),
+					Logger:       defaultLogger(),
+					Host:         "localhost",
+					Port:         8081,
+					ReadTimeout:  10 * time.Second,
+					WriteTimeout: 10 * time.Second,
+					IdleTimeout:  15 * time.Second,
+				}),
+			},
+			want: &server{
+				httpServer: &http.Server{
+					Addr:         "localhost:8081",
+					Handler:      &router{ServeMux: http.NewServeMux()},
+					ReadTimeout:  10 * time.Second,
+					WriteTimeout: 10 * time.Second,
+					IdleTimeout:  15 * time.Second,
+				},
+				router:          &router{ServeMux: http.NewServeMux()},
+				log:             defaultLogger(),
+				shutdownTimeout: defaultShutdownTimeout,
+				shutdownHook:    defaultShutdownHook,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := New(test.input...)
+			if got == nil {
+				t.Errorf("New(%v) = nil; want %v", test.input, test.want)
+			}
+
+			if diff := cmp.Diff(
+				test.want,
+				got,
+				cmp.AllowUnexported(server{}),
+				cmpopts.IgnoreUnexported(http.Server{}, http.ServeMux{}, slog.Logger{}),
+				cmpopts.IgnoreFields(server{}, "shutdownHook", "mu"),
+			); diff != "" {
+				t.Errorf("New(%v) = unexpected result (-want +got):\n%s\n", test.input, diff)
+			}
+		})
+	}
+}
+
+func TestServer_Start(t *testing.T) {
+	t.Run("start server", func(t *testing.T) {
+		shutdownCh := make(chan os.Signal)
+		go func() {
+			time.Sleep(time.Millisecond * 200)
+			shutdownCh <- syscall.SIGINT
+		}()
+
+		var buf bytes.Buffer
+		srv := New(WithLogger(slog.New(slog.NewJSONHandler(&buf, nil))))
+		srv.shutdownHook = func() os.Signal {
+			return <-shutdownCh
+		}
+
+		if gotErr := srv.Start(t.Context()); gotErr != nil {
+			t.Errorf("Start() = unexpected result, got error: %v\n", gotErr)
+		}
+	})
+}
 
 func TestServer_Start_Error(t *testing.T) {
 	t.Run("start server", func(t *testing.T) {
@@ -115,14 +116,20 @@ func TestServer_Start_Error(t *testing.T) {
 		}()
 
 		var buf bytes.Buffer
-		srv := New(WithLogger(slog.New(slog.NewJSONHandler(&buf, nil))))
+		srv := New(
+			WithOptions(Options{
+				Host:   "0.0.0.0",
+				Port:   8090,
+				Logger: slog.New(slog.NewJSONHandler(&buf, nil)),
+			}),
+		)
 		srv.shutdownHook = func() os.Signal {
 			return <-shutdownCh
 		}
 
 		go func() {
 			httpServer := &http.Server{
-				Addr: "0.0.0.0:8080",
+				Addr: "0.0.0.0:8090",
 			}
 			go func() {
 				time.Sleep(time.Millisecond * 200)
@@ -137,7 +144,7 @@ func TestServer_Start_Error(t *testing.T) {
 			t.Errorf("Start() = nil; want error")
 		}
 
-		wantErr := errors.New("listen tcp 0.0.0.0:8080: bind: address already in use")
+		wantErr := errors.New("listen tcp 0.0.0.0:8090: bind: address already in use")
 		if diff := cmp.Diff(wantErr.Error(), gotErr.Error()); diff != "" {
 			t.Errorf("Start() = unexpected result (-want +got):\n%s\n", diff)
 		}
