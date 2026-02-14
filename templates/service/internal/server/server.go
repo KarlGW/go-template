57d6b3ec7ad335e71c252af447cc511621072291
Karl Wallenius <kgw-dev@pm.me>
2026-02-14 21:03:04 +0100

    feat(service): update start and shutdown handling

--- templates/service/internal/server/server.go
+++ templates/service/internal/server/server.go
@@ -1,26 +1,44 @@
 package server
 
 import (
+	"context"
 	"log/slog"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+)
+
+// State represents a server state.
+type State int
+
+const (
+	StateStopped State = iota
+	StateRunning
 )
 
 // server ...
 type server struct {
-	log    *slog.Logger
-	stopCh chan os.Signal
-	errCh  chan error
+	log          *slog.Logger
+	shutdownHook func() os.Signal
+	shutdownCh   chan *shutdownResult
+	errCh        chan error
+	state        State
+	mu           sync.Mutex
 }
 
 // New returns a new server.
 func New(options ...Option) *server {
-	s := &server{}
+	s := &server{
+		state: StateStopped,
+	}
 	for _, option := range options {
 		option(s)
 	}
 
+	if s.shutdownHook == nil {
+		s.shutdownHook = defaultShutdownHook
+	}
 	if s.log == nil {
 		s.log = defaultLogger()
 	}
@@ -28,65 +46,109 @@ func New(options ...Option) *server {
 	return s
 }
 
-type stopResult struct {
-	signal os.Signal
-	err    error
-}
-
 // Start the server.
-func (s server) Start() error {
-	stopCh := make(chan stopResult, 1)
-	errCh := make(chan error, 1)
+func (s *server) Start(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	s.shutdownCh = make(chan *shutdownResult)
+	s.errCh = make(chan error)
+
+	sr := &shutdownResult{}
 	defer func() {
-		close(stopCh)
-		close(errCh)
+		close(s.shutdownCh)
+		close(s.errCh)
+
+		s.log.Info("Server stopped.", "reason", sr.reason())
 	}()
 
 	go func() {
-		s.stop(stopCh)
-	}()
-
-	go func() {
-		// Add server startup code here.
-		// Send errors to s.errCh.
-	}()
-
-	s.log.Info("Server started.")
-	select {
-	case err := <-errCh:
-		return err
-	case sr := <-stopCh:
-		if sr.err != nil {
-			s.log.Info("Error shutting down server.", "error", sr.err)
+		sr.setSignal(s.shutdownHook())
+		if s.State() != StateStopped {
+			s.shutdownCh <- sr
 		}
-		s.log.Info("Server stopped.", "reason", sr.signal.String())
-		return nil
+	}()
+
+	if err := s.startup(ctx); err != nil {
+		return err
 	}
+
+	s.setState(StateRunning)
+	s.log.Info("Server started.")
+
+	<-s.shutdownCh
+	if err := s.shutdown(ctx, sr); err != nil {
+		return err
+	}
+	s.setState(StateStopped)
+	return nil
 }
 
-// stop the server.
-func (s server) stop(stop chan stopResult) {
-	signals := [3]os.Signal{
-		os.Interrupt,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-	}
+// State of the server.
+func (s *server) State() State {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.state
+}
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, signals[:]...)
-	sig := <-interrupt
-	// Reset signals so that a second interrupt will force shutdown.
-	signal.Reset(signals[:]...)
+// startup runs the startup sequence of the server.
+func (s *server) startup(ctx context.Context) error {
+	return nil
+}
 
-	sr := stopResult{
-		signal: sig,
-	}
+// shutdown runs the shutdown sequence of the server.
+func (s *server) shutdown(ctx context.Context, sr *shutdownResult) error {
+	return nil
+}
 
-	// Add server shutdown logic here.
-
-	stop <- sr
+// setState sets the state of the server.
+func (s *server) setState(state State) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state = state
 }
 
 func defaultLogger() *slog.Logger {
 	return slog.New(slog.NewJSONHandler(os.Stderr, nil))
+}
+
+// shutdownSignals returns shutdown signals.
+func shutdownSignals() []os.Signal {
+	return []os.Signal{
+		os.Interrupt,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	}
+}
+
+// defaultShutdownHook is the default shutdown hook for the server,
+// makes use of the default shutdown signals.
+func defaultShutdownHook() os.Signal {
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, shutdownSignals()...)
+	sig := <-shutdown
+	signal.Reset(shutdownSignals()...)
+	return sig
+}
+
+type shutdownResult struct {
+	signal os.Signal
+	mu     sync.Mutex
+}
+
+func (s *shutdownResult) setSignal(signal os.Signal) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.signal = signal
+}
+
+func (s *shutdownResult) reason() string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.signal != nil {
+		return s.signal.String()
+	}
+
+	return "error"
 }
